694bd9290645ee1b5f3a308958d6a7dbea4fa0e5
startec-official <startec.innovations@gmail.com>
2020-12-14 15:08:16 +0800

    added qr decryption

---
+++ src/app/utils/crypto.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import * as CryptoJS from 'crypto-js';  
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CryptoService {
+
+  constructor() { }
+
+  decryptQr( input : string ) {
+    let ciphertext : string = input.substring(0,input.length-4); // UPGRADE : custom key lengths
+    let key : string = input.substring(input.length - 4 , input.length);
+    return CryptoJS.AES.decrypt( ciphertext , key ).toString(CryptoJS.enc.Utf8);
+  }
+}
--- src/app/verify/verify.component.ts
+++ src/app/verify/verify.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { CryptoService } from '../utils/crypto.service';
 import { HttpService } from '../utils/http.service';
 import { User } from '../utils/user';
 
@@ -17,14 +18,17 @@ export class VerifyComponent implements OnInit {
 
   constructor( private route : ActivatedRoute,
                private router : Router,
-               private httpService : HttpService ) { }
+               private httpService : HttpService,
+               private cryptoService : CryptoService ) { }
 
   ngOnInit(): void {
     this.dataReady = false;
     this.currentUser = new User('',-1,'','','','');
     this.route.params.subscribe(( data ) => {
       // TODO : decode the data using the last 4 characters
-      const parsedData : string[] = data.input.toString().split('|'); // UPGRADE : set delimiter to custom
+      let parsedString = this.cryptoService.decryptQr( data.input.toString() );
+      console.log( parsedString );
+      const parsedData : string[] = parsedString.split('|'); // UPGRADE : set delimiter to custom
       this.currentUser.name = parsedData[0];
       this.currentUser.age = parseInt(parsedData[1]);
       this.currentUser.birthdate = parsedData[2];
